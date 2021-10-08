d390acb3c37e2d645c81578ae6dbe528bdb989b0
AxelMies <75790318+AxelMies@users.noreply.github.com>
2021-10-08 14:18:38 +0300

    vaihda numerolista ei-vakioksi

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-  const express = require(´express´)
-+ const
+const express = require(´express´)
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+function arvoNumero(alaraja, ylaraja) {
+  return Math.floor(ylaraja * Math.random()) + alaraja
+}
+
+
+app.get('/', function (req, res) {
+  let numerolista = [
+  arvoNumero(0,5),
+  arvoNumero(10,15),
+  arvoNumero(100,500),
+  arvoNumero(10,20),
+  arvoNumero(0,5),
+]
+    res.json(numerolista)
+})
+  
+app.listen(PORT)
